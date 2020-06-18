9debd0aadb9896ded7a457ea7e6e3dc0733cc98e
NoNick <aleksandrs634@gmail.com>
2020-06-18 11:31:49 +0000

    initial

---
+++ src/main/java/com/example/goragallery/GoragalleryApplication.java
@@ -0,0 +1,32 @@
+package com.example.goragallery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class GoragalleryApplication {
+	@Autowired
+	private
+	Environment env;
+
+	@Bean
+	public DataSource dataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("driverClassName"));
+		dataSource.setUrl(env.getProperty("url"));
+		dataSource.setUsername(env.getProperty("user"));
+		dataSource.setPassword(env.getProperty("password"));
+		return dataSource;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoragalleryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/goragallery/GoragalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goragallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoragalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
